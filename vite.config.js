883b244e507946b2b5963de874fa2837aa8cb7a5
Ivan Tyabin <Ivan_Tyabin@mail.ru>
2025-07-02 11:49:07 +0700

    try fix git pages 2

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/StressTrainer/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/StressTrainer/',
 })
